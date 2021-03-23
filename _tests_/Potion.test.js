7e708e16fd88c15f6d9c7e3de15524c89e0f5d01
savbennett8 <email@example.com>
2021-03-22 23:18:30 -0600

    Potion.js is failing test, not sure why

---
+++ _tests_/Potion.test.js
@@ -0,0 +1,12 @@
+const { test, expect } = require('@jest/globals');
+const Potion = require('../lib/Potion.js');
+
+test('creates a health potion object', () => {
+    //creates a 'new' potion object to store it's name and value
+    //Capitalization of 'Potion' indicates it's a constructor funciton
+    const potion = new Potion('health');
+
+    expect(potion.name).toBe('health');
+    //'expect.any()' method takes a constructor as an argument
+    expect(potion.value).toBe(expect.any(Number));
+});
